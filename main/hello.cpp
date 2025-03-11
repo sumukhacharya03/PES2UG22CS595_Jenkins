311ceefbf20833fc0a5ed64b30f1a6dda7b2f6af
sumukhacharya03 <sumukh.acharya@gmail.com>
2025-03-11 11:02:19 +0530

    Updated hello.cpp File

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, I am Sumukh Acharya\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;

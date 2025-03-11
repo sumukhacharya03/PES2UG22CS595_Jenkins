246cec3107010aa3d51870eca38489d9c79aefcf
sumukhacharya03 <sumukh.acharya@gmail.com>
2025-03-11 11:53:28 +0530

    Added a new CPP File

---
+++ main/test.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, I am Sumukh Acharya\n";
+  cout << "This is a test CPP File\n";
+  cout << "I have successfully built and tested it  \n";
+  return 0;
+}
